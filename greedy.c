f55f23e8c7c5b47e97c67ae5bddc1317f377e87e
Aksana Kuzmitskaya <aksana.kuzmitskaya@gmail.com>
2014-08-20 14:59:38 -0400

    Update greedy.c

--- greedy.c
+++ greedy.c
@@ -14,6 +14,7 @@
 int main(void)
 {
     float change; int amount;
+    
     do 
     {
         printf("O hai! How much change is owed?\n");
@@ -21,22 +22,28 @@ int main(void)
         amount = round(change * 100);
     }
     while (change <= 0);
+    
     int count = 0; int quarter = 25; int dime = 10; int nickel = 5; 
+    
     while (amount % quarter != amount)
     {
         amount -= quarter;
         count++;
     }
+    
     while (amount % dime != amount)
     {
         amount -= dime;
         count++;
     }
+    
     while (amount % nickel != amount)
     {
         amount -= nickel;
         count++;
     }
+    
     count += amount; 
+    
     printf ("%d\n", count);
 }
